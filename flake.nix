8dff6b9c3f533ffef81ba6928e10480afddab73b
madosuki <yurikasamaaikatsu@gmail.com>
2025-12-02 13:31:56 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+        {
+          devShells.default = with pkgs; mkShell {
+            buildInputs = [
+              uv
+              pkg-config
+              openssl
+              sqlite
+              zlib
+              readline
+              xz
+              libffi
+              expat
+              stdenv.cc.cc.lib
+              bzip2
+              libxml2
+              util-linux
+            ];
+            shellHook = ''
+            exec zsh
+            '';
+          };
+        }
+    );
+}
